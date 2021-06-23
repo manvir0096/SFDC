cabb0d766a6ae165e5456d581faf130773c0758c
Manvir Bhatti <manvir@gscloudsolutions.com>
2021-06-22 22:52:19 -0700

    updates

--- SLD/force-app/main/default/lwc/accountList/accountList.js
+++ SLD/force-app/main/default/lwc/accountList/accountList.js
@@ -3,7 +3,7 @@ import getfinanceAccountList from '@salesforce/apex/AccountFinanace.getfinanceAc
 
 const columns = [
     { label: 'Account Name', fieldName: 'Name', sortable: true, type: 'text', editable: true },
-    { label: 'Account Owner ', fieldName: 'AccountOwner', sortable: true, type: 'text', editable: true },
+    { label: 'Account Owner ', fieldName: 'AccountOwner', sortable: true, type: 'text', editable: false },
     { label: 'Phone', fieldName: 'Phone', type: 'phone', editable: true },
     { label: 'Website', fieldName: 'Website', type: 'text', editable: true },
     { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency', editable: true },
@@ -11,7 +11,7 @@ const columns = [
 
 export default class AccountList extends LightningElement {
 
-    @track accounts;
+    @track accounts = [];
     @track error;
 
     accountList;
@@ -19,23 +19,32 @@ export default class AccountList extends LightningElement {
     columns = columns;
     rowOffset = 0;
     defaultSortDirection = 'asc';
-    sortDirection = 'asc';
+    sortedDirection = 'asc';
     sortedBy;
 
     @wire(getfinanceAccountList)
     wiredAccounts({ error, data }) {
         if (data) {
-            this.accounts = data;
+           // this.accounts = data;
+            console.log('---original data--',JSON.parse(JSON.stringify(data)));
+            let d = [];
+            data.forEach(res =>{
+                let account = {};
+                account.Id = res.Id;
+                account.Name = res.Name;
+                account.AccountOwner = res.Owner.Name;
+                account.Phone = res.Phone;
+                account.Website = res.Website;
+                account.AnnualRevenue = res.AnnualRevenue; 
+                account.Industry = res.Industry;
+                d.push(account);
 
+            });
+            this.accounts = d;
             this.error = undefined;
-            console.log(this.accounts);
-            // this.accounts.forEach((res) => {
-            //     res.AccountOwner = res.Owner.Name;
-            // });
-            // Object.preventExtensions(accounts);
-
-
-            this.accountList = accounts;
+            console.log(JSON.parse(JSON.stringify(this.accounts)));
+           // this.mapAccountOwner();
+            this.accountList = this.accounts;
         } else if (error) {
             this.error = error;
             this.accounts = undefined;
@@ -43,26 +52,39 @@ export default class AccountList extends LightningElement {
     }
 
     onHandleSort(event) {
-        const { fieldName: sortedBy, sortDirection } = event.detail;
+    
+        this.sortData(event.detail.fieldName, event.detail.sortDirection);
 
-        console.log(event);
-        const cloneData = [...this.accounts];
+    }
 
-        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
-        this.accounts = cloneData;
-        this.sortDirection = sortDirection;
-        this.sortedBy = sortedBy;
+    sortData(fieldname, direction) {
+       
+        let parseData = JSON.parse(JSON.stringify(this.accounts));
+
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+
+        let isReverse = direction === 'asc' ? 1 : -1;
+
+        // sorting data 
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : '';
+            y = keyValue(y) ? keyValue(y) : '';
+
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+
+        this.accounts = parseData;
+
     }
 
     onNameSearch(event) {
-        console.log(event);
         let searchValue = event.target.value;
-        console.log(searchValue);
-        const cloneData = [...this.accountList];
-
-
+        const cloneData = this.accountList;
         const searchresult = cloneData.filter(function (item) {
-            return item.Name.includes(searchValue);
+            return item.Name.toLowerCase().includes(searchValue.toLowerCase());
         });
         this.accounts = searchresult;
         console.log(searchresult);
